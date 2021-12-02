7701ac5e7309224125ee3c1a17a1caf79d9a3b67
Brent Graves <graves.brent@gmail.com>
2021-12-02 09:47:04 -0500

    changing up portfolio.

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -7,13 +7,13 @@ function Resume() {
       <p>
         Download my <a href="https://grave019.github.io/ProfessionalPortfolio/assets/resume/Brent_Graves_Resume.PDF">Resume</a>
         </p>
-        <h3>Front-end Proficiencies</h3>
+        <h2>Front-end Proficiencies</h2>
         <ul className="skills">
           <li>HTML</li>
           <li>CSS</li>
           <li>JavaScript</li>
           <li>jQuery</li>
-          <li>responsive design</li>
+          <li>Responsive Design</li>
           <li>React</li>
           <li>Bootstrap</li>
         </ul>
@@ -22,7 +22,7 @@ function Resume() {
           <li>APIs</li>
           <li>Node</li>
           <li>Express</li>
-          <li>MySQL, Sequelize</li>
+          <li>MySQL, MySQL2, Sequelize</li>
           <li>MongoDB, Mongoose</li>
           <li>REST</li>
           <li>GraphQL</li>
